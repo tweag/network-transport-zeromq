1b1a9de38118dc4652891e0a0d6d89686dde9a6a
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-02-18 13:26:12 +0400

    Introduce non-monadic utils for ZMQ.

--- src/System/ZMQ4/Utils.hs
+++ src/System/ZMQ4/Utils.hs
@@ -1,5 +1,6 @@
 module System.ZMQ4.Utils
   ( bindFromRangeRandom
+  , bindFromRangeRandomM
   )
   where
 
@@ -7,21 +8,48 @@ import Control.Monad.Catch
 import System.Random ( randomRIO )
 import Text.Printf
 
-import System.ZMQ4.Monadic
+
+import           System.ZMQ4.Monadic
+      ( ZMQ 
+      )
+import qualified System.ZMQ4.Monadic as M
+import           System.ZMQ4
+      ( Context
+      , errno
+      )
+import qualified System.ZMQ4         as P
 
 -- | Bind socket to the random port in a given range.
-bindFromRangeRandom :: Socket z t
+bindFromRangeRandomM :: M.Socket z t
+                     -> String -- ^ Address
+                     -> Int    -- ^ Min port
+                     -> Int    -- ^ Max port
+                     -> Int    -- ^ Max tries
+                     -> ZMQ z Int
+bindFromRangeRandomM sock addr mI mA tr = go tr
+    where
+      go 0 = error "!" -- XXX: throw correct error
+      go x = do
+        port   <- M.liftIO $ randomRIO (mI,mA)
+        result <- try $ M.bind sock (printf "%s:%i" addr port)
+        case result of
+            Left e
+              | errno e == -1 -> go (x - 1)
+              | otherwise -> throwM e
+            Right () -> return port
+
+bindFromRangeRandom :: P.Socket t
                     -> String -- ^ Address
                     -> Int    -- ^ Min port
                     -> Int    -- ^ Max port
                     -> Int    -- ^ Max tries
-                    -> ZMQ z Int
+                    -> IO Int
 bindFromRangeRandom sock addr mI mA tr = go tr
     where
       go 0 = error "!" -- XXX: throw correct error
       go x = do
-        port   <- liftIO $ randomRIO (mI,mA)
-        result <- try $ bind sock (printf "%s:%i" addr port)
+        port   <- randomRIO (mI,mA)
+        result <- try $ P.bind sock (printf "%s:%i" addr port)
         case result of
             Left e
               | errno e == -1 -> go (x - 1)
