0c64d54601b0207078fe599bfdf2c9df5adf036d
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-03-20 11:01:00 +0400

    Add more connection break tests.

--- tests/TestAPI.hs
+++ tests/TestAPI.hs
@@ -11,6 +11,7 @@ import System.Exit
 
 main :: IO ()
 main = finish <=< trySome $ do
+    putStr "simple: "
     Right (zmq, transport) <- createTransportEx defaultZMQParameters "127.0.0.1"
     Right ep1 <- newEndPoint transport
     Right ep2 <- newEndPoint transport
@@ -20,34 +21,48 @@ main = finish <=< trySome $ do
     Right _   <- send c2 ["321"]
     close c1
     close c2
-    m1 <- replicateM 3 $ receive ep1
-    m2 <- replicateM 3 $ receive ep2
-    print m1
-    print m2
-    -- Right ep1 <- newEndPoint transport
-    -- Right ep2 <- newEndPoint transport
-    breakConnection zmq (address ep1) (address ep2)
-    -- closeEndPoint ep2
-    -- Left _  <- connect ep1 (address ep2) ReliableOrdered defaultConnectHints
-    print <=< receive $ ep1
-    print <=< receive $ ep2
-    return ()
+    [ConnectionOpened 1 ReliableOrdered _, Received 1 ["321"], ConnectionClosed 1] <- replicateM 3 $ receive ep1
+    [ConnectionOpened 1 ReliableOrdered _, Received 1 ["123"], ConnectionClosed 1] <- replicateM 3 $ receive ep2
+    putStrLn "OK"
 
-    putStrLn "multicast:"
+    putStr "connection break: "
+    Right ep3 <- newEndPoint transport
+
+    Right c21  <- connect ep1 (address ep2) ReliableOrdered defaultConnectHints
+    Right c22  <- connect ep2 (address ep1) ReliableOrdered defaultConnectHints
+    Right c23  <- connect ep3 (address ep1) ReliableOrdered defaultConnectHints
+
+    ConnectionOpened 2 ReliableOrdered _ <- receive ep2
+    ConnectionOpened 2 ReliableOrdered _ <- receive ep1
+    ConnectionOpened 3 ReliableOrdered _ <- receive ep1
+
+    breakConnection zmq (address ep1) (address ep2)
+
+    ErrorEvent (TransportError (EventConnectionLost _ ) _) <- receive $ ep1
+    ErrorEvent (TransportError (EventConnectionLost _ ) _) <- receive $ ep2
+
+    Left (TransportError SendFailed _) <- send c21 ["test"]
+    Left (TransportError SendFailed _) <- send c22 ["test"]
+
+    Right () <- send c23 ["test"]
+    Received 3 ["test"] <- receive ep1
+    putStrLn "OK"
+
+    putStr "multicast: "
     Right g1 <- newMulticastGroup ep1
     multicastSubscribe g1
     threadDelay 1000000
     multicastSend g1 ["test"]
-    print =<< receive ep1
+    ReceivedMulticast _ ["test"] <- receive ep1
     Right g2 <- resolveMulticastGroup ep2 (multicastAddress g1)
     multicastSubscribe g2
     threadDelay 100000
     multicastSend g2 ["test-2"]
-    print =<< receive ep2
-    print =<< receive ep1
+    ReceivedMulticast _ ["test-2"] <- receive ep2
+    ReceivedMulticast _ ["test-2"] <- receive ep1
+    putStrLn "OK"
     
-    --
-    putStrLn "auth:"
+    putStr "Auth:"
     Right tr2 <- createTransport
                    defaultZMQParameters {authorizationType=ZMQAuthPlain "user" "password"}
                    "127.0.0.1"
@@ -55,16 +70,15 @@ main = finish <=< trySome $ do
     Right ep4 <- newEndPoint tr2
     Right c3  <- connect ep3 (address ep4) ReliableOrdered defaultConnectHints
     Right _   <- send c3 ["4456"]
-    print <=< replicateM 2 $ receive ep4
+    [ConnectionOpened 1 ReliableOrdered _, Received 1 ["4456"]] <- replicateM 2 $ receive ep4
     Right c4  <- connect ep3 (address ep4) ReliableOrdered defaultConnectHints
     Right _   <- send c4 ["5567"]
-    print <=< replicateM 2 $ receive ep4
+    [ConnectionOpened 2 ReliableOrdered _, Received 2 ["5567"]] <- replicateM 2 $ receive ep4
+    putStrLn "OK"
 
-    putStrLn "test no host"
-    er <- connect ep3 (EndPointAddress "tcp://128.0.0.1:7689") ReliableOrdered defaultConnectHints
-    case er of
-      Right c -> error "connection created"
-      Left  e -> print e
+    putStr "Connect to non existing host: "
+    Left (TransportError ConnectFailed _) <- connect ep3 (EndPointAddress "tcp://128.0.0.1:7689") ReliableOrdered defaultConnectHints
+    putStrLn "OK"
   where
     finish (Left e) = print e >> exitWith (ExitFailure 1)
     finish Right{} = exitWith ExitSuccess
